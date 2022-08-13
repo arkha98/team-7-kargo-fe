890d3104682b9b8794d4d79245ecb1b58c565d03
illiyyin <iilliyyin@gmail.com>
2022-08-13 13:33:24 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -6,21 +6,35 @@ import Login from './pages/login';
 import Shipper from './pages/shipper';
 import { AppProvider } from "./utils/AppContext";
 import SideMenu from "./components/SideMenu";
+import { Layout } from "antd";
 // import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
-  const location =useLocation()
+  const location = useLocation()
   const [role, setRole] = useState("shipper")
   useEffect(() => {
     // if (!role) ("/login")
-console.log(location)
+    console.log(location)
   }, [])
   return (
     <AppProvider value={{ role, setRole }}>
 
-      
-        <div className="flex min-h-screen">
-          {location.pathname!="/login"&&<SideMenu />}
+
+      <div className="flex min-h-screen">
+        <Layout>
+        <Layout.Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+          >
+            
+          {location.pathname != "/login" && <SideMenu />}
+    </Layout.Sider>
           <div className="w-full">
 
             <Routes>
@@ -41,8 +55,9 @@ console.log(location)
 
             </Routes>
           </div>
-        </div>
-      
+        </Layout>
+      </div>
+
     </AppProvider>
   );
 }
